f3c02acafbfb4493152370384823e3ee49507980
bkd22 <bkd22@localhost>
2026-08-12 17:43:42 +0500

    Use env vars for DB config and add env example

--- admin/config.php
+++ admin/config.php
@@ -1,17 +1,15 @@
 <?php
-define('db_server', 'localhost');
-define('db_user', 'root');
-define('db_pwd', '');
-define('db_name', 'shop');
+$db_server = getenv('DB_SERVER') ?: 'localhost';
+$db_user = getenv('DB_USER') ?: 'root';
+$db_pwd = getenv('DB_PWD') ?: '';
+$db_name = getenv('DB_NAME') ?: 'shop';
 
+$con = mysqli_connect($db_server, $db_user, $db_pwd, $db_name);
 
-$con = mysqli_connect(db_server, db_user, db_pwd, db_name);
-
-if($con === false){
+if ($con === false) {
     die(mysqli_connect_error());
 } else {
     // echo "Connection Successful";
 }
-
 
 ?>
